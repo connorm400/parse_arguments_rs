5af21238c077575ed4925ff472829619b087fefb
connorm400 <125999884+connorm400@users.noreply.github.com>
2023-09-11 21:04:10 -0600

    added readme to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//!# parse_arguments_rs
+//!Easy way to deal with parsing commandline arguments
+//!
+//!Can be used with any type that implements FromStr (for parsing).
+//!Use `parse_argument()` function to find a value for a specified key (flag). Look at the examples for an idea of how to use it. 
+//!It works for arguments that look like this: 
+//!
+//!```bash
+//!./rust_code --setting=foo --num=42 --hello="world"
+//!```
+//!And to retrieve those values you would write:
+//!```rust
+// assuming you made an Setting enum that implemented FromStr trait
+//!let _ = parse_argument::<Setting>("option").unwrap().unwrap();
+//!let _ = parse_argument::<i32>("num").unwrap().unwrap();
+//!let _ = parse_argument::<String>("hello").unwrap().unwrap();
+//!```
+//!
+//!Run `cargo doc --open` to see the documentation.
+//! 
+//!_Will Hopefully be on [crates.io](https://crates.io) soon  | [Github repo](https://github.com/connorm400/parse_arguments_rs/)_
+//!
+//!## TODO
+//!* the key nor the value can have any spaces because of the nature of `std::env::args`. I will try to fix it at some point.
+//!* Make a function that returns a hashmap of all the arguments with their key and value
+
 use std::env::args;
 use std::str::FromStr;
 
